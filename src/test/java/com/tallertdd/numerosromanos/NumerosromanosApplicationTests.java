82c978acc79bad139ab4a843c6385c6c96d0acfb
matza <matza@192.168.0.4>
2020-04-01 12:30:48 -0300

    Primer commit

---
+++ src/test/java/com/tallertdd/numerosromanos/NumerosromanosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tallertdd.numerosromanos;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumerosromanosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
