91d64388161ce27349990afaf274a39ccb03dcf3
matza <matza@192.168.0.4>
2020-04-01 13:11:45 -0300

    Primer test

--- src/test/java/com/tallertdd/numerosromanos/service/TraductorNumerosRomanosServiceTest.java
+++ src/test/java/com/tallertdd/numerosromanos/service/TraductorNumerosRomanosServiceTest.java
@@ -3,6 +3,17 @@ package com.tallertdd.numerosromanos.service;
 import static org.assertj.core.api.Java6Assertions.assertThat;
 import org.junit.Test;
 
+/**
+ * Pasos:
+ * 1 - Escribir la prueba y que falle
+ * 2 - Escribir el mínimo código productivo para que pasen los test
+ * 3 - Refactorizar el código
+ * 
+ * Reglas:
+ * 1 - No se puede escribir código productivo, a menos que sea para hacer pasar un test fallido.
+ * 2 - No se puede escribir más que lo necesario para que falle un test unitario.
+ * 3 - No se puede escribir más código productivo del estrictamente necesario para hacer pasar un test.
+ */
 public class TraductorNumerosRomanosServiceTest {
     
     @Test
